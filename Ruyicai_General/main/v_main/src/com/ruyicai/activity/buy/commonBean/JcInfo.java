cc625e4c8bc9c9b0acbc765e5641499753addac3
zxflimit <zxflimit@gmail.com>
2013-05-06 11:27:15 +0800

    add common bean

---
+++ Ruyicai_General/main/v_main/src/com/ruyicai/activity/buy/commonBean/JcInfo.java
@@ -0,0 +1,122 @@
+package com.ruyicai.activity.buy.commonBean;
+
+public class JcInfo {
+	String time = "";
+	String day = "";
+	String team = "";
+	String home = "";
+	String away = "";
+	String homeScore = "";
+	String guestScroe = "";
+	String peiLv = "";
+	String timeEnd = "";
+	String week = "";
+	String teamId = "";
+	String letPoint = "";
+	String result = "";
+	String weekId = "";
+
+	public String getWeekId() {
+		return weekId;
+	}
+
+	public void setWeekId(String weekId) {
+		this.weekId = weekId;
+	}
+	public String getPeiLv() {
+		return peiLv;
+	}
+
+	public void setPeiLv(String peiLv) {
+		this.peiLv = peiLv;
+	}
+	
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+	public String getTeamId() {
+		return teamId;
+	}
+
+	public void setTeamId(String teamId) {
+		this.teamId = teamId;
+	}
+
+	public String getLetPoint() {
+		return letPoint;
+	}
+
+	public void setLetPoint(String letPoint) {
+		this.letPoint = letPoint;
+	}
+
+	public String getWeek() {
+		return week;
+	}
+
+	public void setWeek(String week) {
+		this.week = week;
+	}
+
+	public String getDay() {
+		return day;
+	}
+
+	public void setDay(String day) {
+		this.day = day;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
+	}
+
+	public String getHome() {
+		return home;
+	}
+
+	public void setHome(String home) {
+		this.home = home;
+	}
+
+	public String getAway() {
+		return away;
+	}
+
+	public void setAway(String away) {
+		this.away = away;
+	}
+
+	public String getHomeScore() {
+		return homeScore;
+	}
+
+	public void setHomeScore(String score) {
+		this.homeScore = score;
+	}
+
+	public String getGuestScore() {
+		return guestScroe;
+	}
+
+	public void setGuestScore(String score) {
+		this.guestScroe = score;
+	}
+	
+}
