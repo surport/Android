1abd3b4d2a4632f31e28ecc27571f9b7b877da22
zxflimit <zxflimit@gmail.com>
2013-05-28 14:59:18 +0800

    modify by fansm

--- Ruyicai_General/main/v_main/src/com/ruyicai/activity/buy/BuyActivity.java
+++ Ruyicai_General/main/v_main/src/com/ruyicai/activity/buy/BuyActivity.java
@@ -193,11 +193,11 @@ public class BuyActivity extends Activity implements OnClickListener {
 		HEIGHT = getWindowManager().getDefaultDisplay().getHeight();// 屏幕的高度
 		viewPagerContainer = (ViewPager) findViewById(R.id.viewpager);
 		ZixuanAndJiXuan.clearBatchCode();
+		initRollingText();
 		initNumber();
 		initLights();
 		// initGallery();
 		initImgView();
-		initRollingText();
 		isShortcut();
 		MobclickAgent.onEvent(this, "goucaidating"); // BY贺思明 点击主导航上的“购彩大厅”。
 	}
@@ -337,7 +337,6 @@ public class BuyActivity extends Activity implements OnClickListener {
 		map13.put("caizhongSetting", shellRW.getStringValue("qlc").toString());
 		caizhongSettingList.add(map13);
 		
-		CheckUtil.checkLotteryTicketSale(isFirstLaunch,Constants.LOTNO_22_5,this);
 		Map<String, String> map14 = new HashMap<String, String>();
 		map14.put("lotno", "T01013");
 		map14.put("caizhongSetting", shellRW.getStringValue("22-5").toString());
@@ -1178,7 +1177,7 @@ public class BuyActivity extends Activity implements OnClickListener {
 		ViewFlipper mFlipper = ((ViewFlipper) this
 				.findViewById(R.id.notice_other_flipper));
 		mFlipper.setOnClickListener(filterclick);
-		if ("".equals(Constants.NEWS)) {
+		if ("".equals(Constants.NEWS) || "".equals(Constants.todayjosn)) {
 			registerIntentReceivers();
 		} else {
 			//TextView text = (TextView) findViewById(R.id.notice_other_title);
@@ -1191,6 +1190,7 @@ public class BuyActivity extends Activity implements OnClickListener {
     private void newsUpdateHandler() {
 		TextView text = (TextView) findViewById(R.id.notice_other_title);
 		text.setText(Constants.NEWS);
+		CheckUtil.checkLotteryTicketSale(isFirstLaunch,Constants.LOTNO_22_5,this);
 		if (newsUpdateReceiver != null) {
 			unregisterIntentReceivers();
 		}
