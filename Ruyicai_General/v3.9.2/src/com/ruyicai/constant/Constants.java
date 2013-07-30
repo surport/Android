5fe5cab706079d2ced6c31192f64b5d969bbdeca
yejc <yejianchun@ruyicai.com>
2013-07-30 14:29:28 +0800

    修改该版本号

--- Ruyicai_General/v3.9.2/src/com/ruyicai/constant/Constants.java
+++ Ruyicai_General/v3.9.2/src/com/ruyicai/constant/Constants.java
@@ -48,7 +48,7 @@ public class Constants {
 	/**
 	 * 打新包需要修改
 	 */
-	public static String SOFTWARE_VERSION = "3.8.3";
+	public static String SOFTWARE_VERSION = "3.9.2";
 	public static String MERCHANT_PACKAGE = "com.palmdream.RuyicaiAndroid";// 商户包名
 	/**
 	 * 新浪分享修改
