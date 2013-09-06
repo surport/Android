5868748f5a0b7c972813f771164f19e07dca0101
chengxiang <zxflimit@gmail.com>
2013-09-06 15:29:49 +0800

    修改了版本号

--- Ruyicai_General/v3.10.1/src/com/ruyicai/constant/Constants.java
+++ Ruyicai_General/v3.10.1/src/com/ruyicai/constant/Constants.java
@@ -48,7 +48,7 @@ public class Constants {
 	/**
 	 * 打新包需要修改
 	 */
-	public static String SOFTWARE_VERSION = "3.10.0";
+	public static String SOFTWARE_VERSION = "3.10.1";
 	public static String MERCHANT_PACKAGE = "com.palmdream.RuyicaiAndroid";// 商户包名
 	/**
 	 * 新浪分享修改
