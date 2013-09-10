e834a4a5c4a0f29f587d5ef7ed3f4a8ee1153434
zxflimit <zxflimit@gmail.com>
2013-09-10 16:49:52 +0800

    modify

--- Ruyicai_General/main/v_main/src/com/ruyicai/net/transaction/FootballInterface.java
+++ Ruyicai_General/main/v_main/src/com/ruyicai/net/transaction/FootballInterface.java
@@ -58,39 +58,4 @@ public class FootballInterface {
 		}
 		return reValue;
 	}
-
-	/**
-	 * 获取足彩的分析信息
-	 * 
-	 * @param lotno
-	 *            彩种
-	 * @param batchCode
-	 *            期号
-	 * @param num
-	 *            对应队列的num
-	 * @return
-	 */
-	public String getZCInfo(String lotno, String batchCode, String num) {
-		String action = "zcAction.do";
-		String method = "getFlDataInfo";
-		String reValue = "";
-		try {
-			String para = "";
-			JSONObject paras = new JSONObject();
-			paras.put("lotno", lotno);
-			paras.put("batchCode", batchCode);
-			paras.put("num", num);
-			para = URLEncoder.encode(paras.toString());
-			String re = InternetUtils
-					.GetMethodOpenHttpConnectJrt(Constants.SERVER_URL + action
-							+ ";jsessionid=" + Constants.sessionId + "?method="
-							+ method + "&jsonString=" + para);
-			if (re != null && re.length() > 0) {
-				reValue = re;
-			}
-		} catch (Exception e) {
-		}
-
-		return reValue;
-	}
 }
