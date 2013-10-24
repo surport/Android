c0c022c1dcfae874c7f3d2591c6b784adbb58720
chengxiang <zxflimit@gmail.com>
2013-10-24 18:21:08 +0800

    modify the button of demaidetail turn to hemai query 	modified:   com/ruyicai/activity/join/JoinDetailActivity.java

--- Ruyicai_General/main/v_main/src/com/ruyicai/activity/join/JoinDetailActivity.java
+++ Ruyicai_General/main/v_main/src/com/ruyicai/activity/join/JoinDetailActivity.java
@@ -1847,6 +1847,7 @@ public class JoinDetailActivity extends Activity implements HandlerMsg {
 			/** add by pengcx 20130725 start*/
 			Intent intent = new Intent(JoinDetailActivity.this,BettingSuccessActivity.class);
 			intent.putExtra("from", BettingSuccessActivity.JOINCOOPERATION);
+			intent.putExtra("page", BettingSuccessActivity.COOPERATION);
 			intent.putExtra("lotno", detatil.getLotNo());
 			intent.putExtra("amount", Integer.valueOf((Integer.valueOf(amount) * 100)).toString());
 			startActivity(intent);
