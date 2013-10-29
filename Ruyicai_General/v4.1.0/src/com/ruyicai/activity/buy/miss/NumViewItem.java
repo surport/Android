8dd853c3274f4527c920eafd2e1364c259c688c2
xupeisong <xupeisong@ruyicai.com>
2013-10-29 13:09:19 +0800

    updata

--- Ruyicai_General/v4.1.0/src/com/ruyicai/activity/buy/miss/NumViewItem.java
+++ Ruyicai_General/v4.1.0/src/com/ruyicai/activity/buy/miss/NumViewItem.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import com.palmdream.RuyicaiAndroid.R;
