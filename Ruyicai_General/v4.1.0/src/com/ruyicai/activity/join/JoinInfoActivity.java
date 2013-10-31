6a99e56c593b4fbab93430210a86a74efc6d1bcb
xupeisong <xupeisong@ruyicai.com>
2013-10-31 13:52:40 +0800

    updata

--- Ruyicai_General/v4.1.0/src/com/ruyicai/activity/join/JoinInfoActivity.java
+++ Ruyicai_General/v4.1.0/src/com/ruyicai/activity/join/JoinInfoActivity.java
@@ -80,6 +80,7 @@ public class JoinInfoActivity extends Activity implements HandlerMsg {
 	Button imgUp, imgDown;
 	String name ;
 	LinearLayout ll_title;
+	private boolean isAddMoreUse;
 	
 	Dialog dialog;
 	RelativeLayout progress, allAtm, atm;// 排序按钮
@@ -329,7 +330,7 @@ public class JoinInfoActivity extends Activity implements HandlerMsg {
 
 	}
 	private void addmore() {
-
+		isAddMoreUse = true;
 		viewInfos[topIndex][lottypeIndex].newPage++;
 		int allpage = viewInfos[topIndex][lottypeIndex].allPage;
 		if (viewInfos[topIndex][lottypeIndex].newPage < viewInfos[topIndex][lottypeIndex].allPage) {
@@ -485,7 +486,7 @@ public class JoinInfoActivity extends Activity implements HandlerMsg {
 	public void onCheck() {
 		viewInfos[topIndex][lottypeIndex].newPage = 0;
 		viewInfos[topIndex][lottypeIndex].allPage = 0;
-		viewInfos[topIndex][lottypeIndex].listdata.clear();
+		
 		joinInfokNet(orderBy, orderDir);
 		view.setEnabled(true);
 	}
@@ -509,6 +510,10 @@ public class JoinInfoActivity extends Activity implements HandlerMsg {
 		str = QueryJoinInfoInterface.queryLotJoinInfo(lotno, issue, orderBy,
 				orderDir, "" + viewInfos[topIndex][lottypeIndex].newPage,
 				Constants.PAGENUM);
+		if(!isAddMoreUse){
+			viewInfos[topIndex][lottypeIndex].listdata.clear();
+		}
+		isAddMoreUse = false;
 		handlerTwo.post(new Runnable() {
 			@Override
 			public void run() {
@@ -680,16 +685,18 @@ public class JoinInfoActivity extends Activity implements HandlerMsg {
 		OnItemClickListener clickListener = new OnItemClickListener() {
 
 			public void onItemClick(AdapterView<?> parent, View view,final int position, long id) {
-				Info info = (Info) viewInfos[topIndex][lottypeIndex].listdata.get(position);
-				if (info.getAtm().equals(info.getAllAtm())) {
-					Toast.makeText(JoinInfoActivity.this, "该方案已经满员,请您选择其他方案！",Toast.LENGTH_SHORT).show();
-				} else {
-					Intent intent = new Intent(JoinInfoActivity.this,JoinDetailActivity.class);
-					intent.putExtra(ID, info.getId());
-					intent.putExtra(Constants.LOTNO, info.getLotno());
-					intent.putExtra(Constants.ISSUE, info.getBatchCode());
-					intent.putExtra(USER_NO, info.getStarterUserNo());
-					startActivity(intent);
+				if(viewInfos[topIndex][lottypeIndex].listdata.size()!=0){
+					Info info = (Info) viewInfos[topIndex][lottypeIndex].listdata.get(position);
+					if (info.getAtm().equals(info.getAllAtm())) {
+						Toast.makeText(JoinInfoActivity.this, "该方案已经满员,请您选择其他方案！",Toast.LENGTH_SHORT).show();
+					} else {
+						Intent intent = new Intent(JoinInfoActivity.this,JoinDetailActivity.class);
+						intent.putExtra(ID, info.getId());
+						intent.putExtra(Constants.LOTNO, info.getLotno());
+						intent.putExtra(Constants.ISSUE, info.getBatchCode());
+						intent.putExtra(USER_NO, info.getStarterUserNo());
+						startActivity(intent);
+					}
 				}
 			}
 
