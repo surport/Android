60ab2926c67ba040ebf0b6a0df4e8f8f33bfbff3
xupeisong <xupeisong@ruyicai.com>
2013-11-01 15:25:20 +0800

    updata

--- Ruyicai_General/main/v_main/src/com/ruyicai/activity/buy/cq11x5/CqElevenFive.java
+++ Ruyicai_General/main/v_main/src/com/ruyicai/activity/buy/cq11x5/CqElevenFive.java
@@ -1,4 +1,5 @@
 package com.ruyicai.activity.buy.cq11x5;
+
 import org.json.JSONObject;
 
 import android.app.AlertDialog;
@@ -18,6 +19,7 @@ import android.widget.TextView;
 import com.palmdream.RuyicaiAndroid.R;
 import com.ruyicai.activity.buy.dlc.Dlc;
 import com.ruyicai.activity.buy.high.ZixuanAndJiXuan;
+import com.ruyicai.code.dlc.DlcCode;
 import com.ruyicai.constant.Constants;
 import com.ruyicai.jixuan.Balls;
 import com.ruyicai.net.newtransaction.GetLotNohighFrequency;
@@ -26,24 +28,25 @@ import com.ruyicai.util.CheckUtil;
 import com.ruyicai.util.PublicMethod;
 
 public class CqElevenFive extends ZixuanAndJiXuan {
-	
-	private  RelativeLayout relativeLayout;
-	private TextView titleOne;//标题
-	private TextView betInfo;//投注提示1
-	private TextView batchcode;//投注提示2
-	private TextView time;//距离开奖时间
-	private CheckBox missCheck;//遗漏值开关
-	private Button mYaoYao;//摇一摇机选
-	private Button imgRetrun;//Top右边按钮
-	private Button refreshBtn;//Top刷新按钮
+
+	private RelativeLayout relativeLayout;
+	private TextView titleOne;// 标题
+	private TextView betInfo;// 投注提示1
+	private TextView batchcode;// 投注提示2
+	private TextView time;// 距离开奖时间
+	private CheckBox missCheck;// 遗漏值开关
+	private Button mYaoYao;// 摇一摇机选
+	private Button imgRetrun;// Top右边按钮
+	private Button refreshBtn;// Top刷新按钮
 	int lesstime;// 剩余时间
 	public static String batchCode;// 期号
 	private boolean isRun = true;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		lotno = Constants.CQELVENFIVE;
+		lotno = Constants.LOTNO_CQ_ELVEN_FIVE;
 		inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		layoutMain = inflater.inflate(R.layout.buy_cq_eleven_five_main, null);
 		setContentView(layoutMain);
@@ -97,17 +100,17 @@ public class CqElevenFive extends ZixuanAndJiXuan {
 	 */
 	private void initView() {
 		relativeLayout = (RelativeLayout) findViewById(R.id.last_batchcode);
-		titleOne = (TextView) findViewById(R.id.layout_main_text_title_one);//标题
-		betInfo = (TextView) findViewById(R.id.bet_info);//投注提示1
-		batchcode = (TextView) findViewById(R.id.last_batchcode_textlable_red);//投注提示2 
-		time = (TextView) findViewById(R.id.layout_main_text_time);//距离开奖时间
-		imgRetrun = (Button) findViewById(R.id.layout_main_img_return);//Top右边按钮
-		refreshBtn = (Button) findViewById(R.id.refresh_code);//Top刷新按钮
-		mYaoYao = (Button) findViewById(R.id.yaoyao_jisuan);//摇一摇机选
-		missCheck=(CheckBox)findViewById(R.id.missCheck);//遗漏值开关
-		
-		titleOne.setText(getString(R.string.cq_11_5));         
-		
+		titleOne = (TextView) findViewById(R.id.layout_main_text_title_one);// 标题
+		betInfo = (TextView) findViewById(R.id.bet_info);// 投注提示1
+		batchcode = (TextView) findViewById(R.id.last_batchcode_textlable_red);// 投注提示2
+		time = (TextView) findViewById(R.id.layout_main_text_time);// 距离开奖时间
+		imgRetrun = (Button) findViewById(R.id.layout_main_img_return);// Top右边按钮
+		refreshBtn = (Button) findViewById(R.id.refresh_code);// Top刷新按钮
+		mYaoYao = (Button) findViewById(R.id.yaoyao_jisuan);// 摇一摇机选
+		missCheck = (CheckBox) findViewById(R.id.missCheck);// 遗漏值开关
+
+		titleOne.setText(getString(R.string.cq_11_5));
+
 		refreshBtn.setVisibility(View.VISIBLE);
 		refreshBtn.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
@@ -117,10 +120,11 @@ public class CqElevenFive extends ZixuanAndJiXuan {
 		imgRetrun.setVisibility(View.VISIBLE);
 		imgRetrun.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-//				createDialog(NoticeActivityGroup.ID_SUB_DLC_LISTVIEW);
+				// createDialog(NoticeActivityGroup.ID_SUB_DLC_LISTVIEW);
 			}
 		});
 	}
+
 	/**
 	 * 赋值给当前期
 	 * 
@@ -142,13 +146,19 @@ public class CqElevenFive extends ZixuanAndJiXuan {
 						JSONObject obj = new JSONObject(re);
 						message = obj.getString("message");
 						error_code = obj.getString("error_code");
-						lesstime = Integer.valueOf(CheckUtil.isNull(obj.getString("time_remaining")));
+						lesstime = Integer.valueOf(CheckUtil.isNull(obj
+								.getString("time_remaining")));
 						batchCode = obj.getString("batchcode");
 						while (isRun) {
 							if (isEnd(lesstime)) {
 								sscHandler.post(new Runnable() {
 									public void run() {
-										time.setText("剩余时间:"+ PublicMethod.isTen(lesstime / 60)+ ":"+ PublicMethod.isTen(lesstime % 60));
+										time.setText("距" + batchCode.substring(8) + "期截止:"
+												+ PublicMethod
+														.isTen(lesstime / 60)
+												+ "分"
+												+ PublicMethod
+														.isTen(lesstime % 60)+"秒");
 									}
 								});
 								Thread.sleep(1000);
@@ -177,6 +187,7 @@ public class CqElevenFive extends ZixuanAndJiXuan {
 		});
 		thread.start();
 	}
+
 	private boolean isEnd(int time) {
 		if (time > 0) {
 			return true;
@@ -184,14 +195,16 @@ public class CqElevenFive extends ZixuanAndJiXuan {
 			return false;
 		}
 	}
-	
+
 	/**
 	 * 转入下一期对话框
 	 */
 	private void nextIssue() {
 		new AlertDialog.Builder(CqElevenFive.this)
 				.setTitle("提示")
-				.setMessage(titleOne.getText().toString() + "第" + batchCode+ "期已经结束,是否转入下一期")
+				.setMessage(
+						titleOne.getText().toString() + "第" + batchCode
+								+ "期已经结束,是否转入下一期")
 				.setNegativeButton("转入下一期", new Dialog.OnClickListener() {
 					public void onClick(DialogInterface dialog, int which) {
 						// TODO Auto-generated method stub
@@ -199,7 +212,8 @@ public class CqElevenFive extends ZixuanAndJiXuan {
 					}
 
 				})
-				.setNeutralButton("返回主页面",new DialogInterface.OnClickListener() {
+				.setNeutralButton("返回主页面",
+						new DialogInterface.OnClickListener() {
 
 							public void onClick(DialogInterface dialog,
 									int which) {
@@ -207,5 +221,109 @@ public class CqElevenFive extends ZixuanAndJiXuan {
 								CqElevenFive.this.finish();
 							}
 						}).create().show();
+	}
+
+	/**
+	 * 投注提示
+	 */
+	private void setBetPrompt(int type) {
+		switch (type) {
+		//普通
+		case 0:
+			betInfo.setText(getString(R.string.cq_11_5_prize_rx_2));
+			batchcode.setText(getString(R.string.cq_11_5_prize2_rx_2));
+			break;
+		case 1:
+			betInfo.setText(getString(R.string.cq_11_5_prize_rx_3));
+			batchcode.setText(getString(R.string.cq_11_5_prize2_rx_3));
+			break;
+		case 2:
+			betInfo.setText(getString(R.string.cq_11_5_prize_rx_4));
+			batchcode.setText(getString(R.string.cq_11_5_prize2_rx_4));
+			break;
+		case 3:
+			betInfo.setText(getString(R.string.cq_11_5_prize_rx_5));
+			batchcode.setText(getString(R.string.cq_11_5_prize2_rx_5));
+			break;
+		case 4:
+			betInfo.setText(getString(R.string.cq_11_5_prize_rx_6));
+			batchcode.setText(getString(R.string.cq_11_5_prize2_rx_6));
+			break;
+		case 5:
+			betInfo.setText(getString(R.string.cq_11_5_prize_rx_7));
+			batchcode.setText(getString(R.string.cq_11_5_prize2_rx_7));
+			break;
+		case 6:
+			betInfo.setText(getString(R.string.cq_11_5_prize_rx_8));
+			batchcode.setText(getString(R.string.cq_11_5_prize2_rx_8));
+			break;
+			//胆拖
+		case 7:
+			betInfo.setText(getString(R.string.cq_11_5_prize_rx_2));
+			batchcode.setText(getString(R.string.cq_11_5_prize2_dt_2));
+			break;
+		case 8:
+			betInfo.setText(getString(R.string.cq_11_5_prize_rx_3));
+			batchcode.setText(getString(R.string.cq_11_5_prize2_dt_3));
+			break;
+		case 9:
+			betInfo.setText(getString(R.string.cq_11_5_prize_rx_4));
+			batchcode.setText(getString(R.string.cq_11_5_prize2_dt_4));
+			break;
+		case 10:
+			betInfo.setText(getString(R.string.cq_11_5_prize_rx_5));
+			batchcode.setText(getString(R.string.cq_11_5_prize2_dt_5));
+			break;
+		case 11:
+			betInfo.setText(getString(R.string.cq_11_5_prize_rx_6));
+			batchcode.setText(getString(R.string.cq_11_5_prize2_dt_6));
+			break;
+		case 12:
+			betInfo.setText(getString(R.string.cq_11_5_prize_rx_7));
+			batchcode.setText(getString(R.string.cq_11_5_prize2_dt_7));
+			break;
+		case 13:
+			betInfo.setText(getString(R.string.cq_11_5_prize_rx_8));
+			batchcode.setText(getString(R.string.cq_11_5_prize2_dt_8));
+			break;
+			//前一
+		case 14:
+			betInfo.setText(getString(R.string.cq_11_5_prize_pt_qy));
+			batchcode.setText(getString(R.string.cq_11_5_prize2_pt_qy));
+			break;
+			//前二直选
+		case 15:
+			betInfo.setText(getString(R.string.cq_11_5_prize_pt_qe_zhix));
+			batchcode.setText(getString(R.string.cq_11_5_prize2_pt_qe_zhix));
+			break;
+			//前二组选
+		case 16:
+			betInfo.setText(getString(R.string.cq_11_5_prize_pt_qe_zux));
+			batchcode.setText(getString(R.string.cq_11_5_prize2_pt_qe_zux));
+			break;
+			//前三直选
+		case 17:
+			betInfo.setText(getString(R.string.cq_11_5_prize_pt_qs_zhix));
+			batchcode.setText(getString(R.string.cq_11_5_prize2_pt_qs_zhix));
+			break;
+		case 18:
+			//前三组选
+			betInfo.setText(getString(R.string.cq_11_5_prize_pt_qs_zux));
+			batchcode.setText(getString(R.string.cq_11_5_prize2_pt_qs_zux));
+			break;
+			//胆拖前二组选
+		case 19:
+			betInfo.setText("");
+			batchcode.setText(getString(R.string.cq_11_5_prize2_dt_qe_zux));
+			break;
+			//胆拖前三组选
+		case 20:
+			betInfo.setText("");
+			batchcode.setText(getString(R.string.cq_11_5_prize2_dt_qs_zux));
+			break;
+
+		default:
+			break;
+		}
 	}
 }
