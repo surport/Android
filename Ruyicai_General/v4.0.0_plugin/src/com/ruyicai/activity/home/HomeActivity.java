34044b0adde1044a6e20ea4611118e871facc6d6
lili <zxflimit@gmail.com>
2013-11-28 09:39:00 +0800

    update ProtocolManager.java

--- Ruyicai_General/v4.0.0_plugin/src/com/ruyicai/activity/home/HomeActivity.java
+++ Ruyicai_General/v4.0.0_plugin/src/com/ruyicai/activity/home/HomeActivity.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 
